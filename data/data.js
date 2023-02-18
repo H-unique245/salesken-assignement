302d862fc8dd4cedb14cfef9d8bf709812d5364b
H-unique245 <a.hemant96@gmail.com>
2023-02-18 14:50:13 +0530

    Login and signup added

---
+++ data/data.js
@@ -0,0 +1,103 @@
+let data=[
+    {
+        "question": "Whistler was the codename of this Microsoft Operating System.",
+        "correct_answer": "Windows XP",
+        "options": [
+            "Windows XP",
+            "Windows 2000",
+            "Windows 7",
+            "Windows 95"
+        ]
+    },
+    {
+        "question": "What was the name given to Android 4.3?",
+        "correct_answer": "Jelly Bean",
+        "options": [
+            "Jelly Bean",
+            "Lollipop",
+            "Nutella",
+            "Froyo"
+        ]
+    },
+    {
+        "question": "How many cores does the Intel i7-6950X have?",
+        "correct_answer": "10",
+        "options": [
+            "10",
+            "12",
+            "8",
+            "4"
+        ]
+    },
+    {
+        "question": "Which coding language was the #1 programming language in terms of usage on GitHub in 2015?",
+        "correct_answer": "JavaScript",
+        "options": [
+            "JavaScript",
+            "C#",
+            "Python",
+            "PHP"
+        ]
+    },
+    {
+        "question": "How fast is USB 3.1 Gen 2 theoretically?",
+        "correct_answer": "10 Gb/s",
+        "options": [
+            "10 Gb/s",
+            "5 Gb/s",
+            "8 Gb/s",
+            "1 Gb/s"
+        ]
+    },
+    {
+        "question": "Which operating system was released first?",
+        "correct_answer": "Mac OS",
+        "options": [
+            "Mac OS",
+            "Windows",
+            "Linux",
+            "OS/2"
+        ]
+    },
+    {
+        "question": "What is known as &quot;the brain&quot; of the Computer?",
+        "correct_answer": "Central Processing Unit",
+        "options": [
+            "Central Processing Unit",
+            "Motherboard",
+            "Graphics Processing Unit",
+            "Keyboard"
+        ]
+    },
+    {
+        "question": "Which of these people was NOT a founder of Apple Inc?",
+        "correct_answer": "Jonathan Ive",
+        "options": [
+            "Jonathan Ive",
+            "Steve Jobs",
+            "Ronald Wayne",
+            "Steve Wozniak"
+        ]
+    },
+    {
+        "question": "Laserjet and inkjet printers are both examples of what type of printer?",
+        "correct_answer": "Non-impact printer",
+        "options": [
+            "Non-impact printer",
+            "Impact printer",
+            "Daisywheel printer",
+            "Dot matrix printer"
+        ]
+    },
+    {
+        "question": "How many bits make up the significand portion of a single precision floating point number?",
+        "correct_answer": "23",
+        "options": [
+            "23",
+            "8",
+            "53",
+            "15"
+        ]
+    }
+]
+export {data}
